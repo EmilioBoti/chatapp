b33f5835f35e2909ad64a303f6ea5aa393ea8d58
Emilio Botier <70599464+EmilioBoti@users.noreply.github.com>
2022-12-09 19:07:13 +0100

    added jwt to notification endpoint

--- src/controller/notification/notification.router.js
+++ src/controller/notification/notification.router.js
@@ -1,11 +1,12 @@
 const express = require("express")
+const { auth } = require("../chatBusiness/chatController")
 const router = express.Router()
 
 const notificationLogic = require("./notification.logic")
 
-router.post("/acceptNotification", notificationLogic.acceptFriendRequest)
-router.post("/rejectNotification", notificationLogic.rejectFriendRequest)
-router.get("/notifications/:id", notificationLogic.getNotifications)
+router.post("/acceptNotification", auth , notificationLogic.acceptFriendRequest)
+router.post("/rejectNotification", auth, notificationLogic.rejectFriendRequest)
+router.get("/notifications/:id", auth, notificationLogic.getNotifications)
 
 
 module.exports = { router }
